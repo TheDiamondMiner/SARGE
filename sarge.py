af0127238c6685ec77183ab662bfc0333e2d47d7
TheDiamondMiner <73134219+TheDiamondMiner@users.noreply.github.com>
2023-12-13 19:47:12 +0530

    Add files via upload

---
+++ sarge.py
@@ -0,0 +1,99 @@
+import pyaudio
+from rev_ai.models import MediaConfig
+from rev_ai.streamingclient import RevAiStreamingClient
+from six.moves import queue
+import json
+
+# Replace 'YOUR_ACCESS_TOKEN' with your actual Rev.ai access token
+access_token = "02TAiLcp-j1gkC-5waYgDkzPXvv3IYhQ4D7gO2v2CFlQtMoMl4bkPsw7WFWxr3fT_3GoCVLmD-PF4ln4GoRPrBxvF5rNU"
+
+class MicrophoneStream(object):
+    def __init__(self, rate, chunk):
+        self._rate = rate
+        self._chunk = chunk
+        self._buff = queue.Queue()
+        self.closed = True
+
+    def __enter__(self):
+        self._audio_interface = pyaudio.PyAudio()
+        self._audio_stream = self._audio_interface.open(
+            format=pyaudio.paInt16,
+            channels=1, rate=self._rate,
+            input=True, frames_per_buffer=self._chunk,
+            stream_callback=self._fill_buffer,
+        )
+        self.closed = False
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self._audio_stream.stop_stream()
+        self._audio_stream.close()
+        self.closed = True
+        self._buff.put(None)
+        self._audio_interface.terminate()
+
+    def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
+        self._buff.put(in_data)
+        return None, pyaudio.paContinue
+
+    def generator(self):
+        while not self.closed:
+            chunk = self._buff.get()
+            if chunk is None:
+                return
+            data = [chunk]
+            while True:
+                try:
+                    chunk = self._buff.get(block=False)
+                    if chunk is None:
+                        return
+                    data.append(chunk)
+                except queue.Empty:
+                    break
+            yield b''.join(data)
+
+def censor_print(text):
+    # List of profane words (add more as needed)
+    profane_words = ["fuck", "motherfucker", "bitch", "bitchass","bitchassnigga","nigga"]
+
+    # Split the text into words
+    words = text.split()
+
+    # Iterate through each word
+    for i in range(len(words)):
+        # Check if the word is in the profane words list
+        if words[i].lower() in profane_words:
+            # Replace the word with asterisks
+            words[i] = '*'
+
+    # Join the words back into a string
+    censored_text = ' '.join(words)
+    
+    # Print the censored text
+    print(censored_text)
+
+
+# Configurations
+rate = 44100
+chunk = int(rate / 10)
+example_mc = MediaConfig('audio/x-raw', 'interleaved', 44100, 'S16LE', 1)
+streamclient = RevAiStreamingClient(access_token, example_mc)
+
+with MicrophoneStream(rate, chunk) as stream:
+    try:
+        response_gen = streamclient.start(stream.generator())
+        full_transcript = ''
+
+        for response in response_gen:
+            response_json = json.loads(response)
+            if response_json['type'] in ('final'):
+                elements = response_json['elements']
+                transcript = ' '.join(elem['value'] for elem in elements if elem['type'] == 'text')
+                full_transcript += transcript
+                print(transcript)  # Print each chunk of transcription
+
+        censor_print(full_transcript)
+
+    except KeyboardInterrupt:
+        streamclient.end()
+        pass
