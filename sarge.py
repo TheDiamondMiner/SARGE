3fa1135b93a8399f7c5780bfc6e7eee1496c5f6d
TheDiamondMiner <73134219+TheDiamondMiner@users.noreply.github.com>
2023-12-15 01:45:48 +0530

    Add files via upload

--- sarge.py
+++ sarge.py
@@ -1,4 +1,5 @@
 import time
+import subprocess
 import pyaudio
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
@@ -59,27 +60,73 @@ access_token = "02Xw3PNnMTiXAe5VENUQKJj3c5RVMMtap8iD8HhzLl0NzjOItgwr4UKTC96h0DFq
 
 # Microphone stream class
 class MicrophoneStream(object):
-    # ... (rest of your existing code)
+    def __init__(self, rate, chunk):
+        self._rate = rate
+        self._chunk = chunk
+        self._buff = queue.Queue()
+        self.closed = True
+
+    def __enter__(self):
+        self._audio_interface = pyaudio.PyAudio()
+        self._audio_stream = self._audio_interface.open(
+            format=pyaudio.paInt16,
+            channels=1, rate=self._rate,
+            input=True, frames_per_buffer=self._chunk,
+            stream_callback=self._fill_buffer,
+        )
+        self.closed = False
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self._audio_stream.stop_stream()
+        self._audio_stream.close()
+        self.closed = True
+        self._buff.put(None)
+        self._audio_interface.terminate()
+
+    def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
+        self._buff.put(in_data)
+        return None, pyaudio.paContinue
+
+    def generator(self):
+        while not self.closed:
+            chunk = self._buff.get()
+            if chunk is None:
+                return
+            data = [chunk]
+            while True:
+                try:
+                    chunk = self._buff.get(block=False)
+                    if chunk is None:
+                        return
+                    data.append(chunk)
+                except queue.Empty:
+                    break
+            yield b''.join(data)
+
 
 # Rev.ai streaming client setup and main execution
-# ... (rest of your existing code)
+rate = 44100
+chunk = int(rate / 10)
+mc = MediaConfig('audio/x-raw', 'interleaved', rate, 'S16LE', 1)
+streamclient = RevAiStreamingClient(access_token, mc)
 
-    try:
-        response_gen = streamclient.start(stream.generator())
-        full_transcript = ''
+try:
+    response_gen = streamclient.start(stream.generator())
+    full_transcript = ''
 
-        for response in response_gen:
-            response_json = json.loads(response)
-            if response_json['type'] in ('final', 'partial'):
-                elements = response_json['elements']
-                transcript = ' '.join(elem['value'] for elem in elements if elem['type'] == 'text')
-                full_transcript += transcript
-                display_console_text(transcript)  # Display text on OLED
+    for response in response_gen:
+        response_json = json.loads(response)
+        if response_json['type'] in ('final', 'partial'):
+            elements = response_json['elements']
+            transcript = ' '.join(elem['value'] for elem in elements if elem['type'] == 'text')
+            full_transcript += transcript
+            display_console_text(transcript)  # Display text on OLED
 
-        # Clear the display after all lines are shown
-        draw.rectangle((0, 0, width, height), outline=0, fill=0)
-        disp.image(image)
-        disp.display()
+    # Clear the display after all lines are shown
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    disp.image(image)
+    disp.display()
 
-    except KeyboardInterrupt:
-        streamclient.end()
+except KeyboardInterrupt:
+    streamclient.end()
